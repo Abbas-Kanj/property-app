2942fc13794cbd218730fdd25f5b41a33221ccce
Abbas <abbas_kanj@hotmail.com>
2024-06-25 20:47:23 +0300

    modified the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Link href="properties">Go to properties</Link>
+    </div>
+  );
 };
 
 export default HomePage;
