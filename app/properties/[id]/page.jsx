d7f5e909e4fefd2ff4a4d87a6a459e83f064e5a6
Abbas <abbas_kanj@hotmail.com>
2024-07-01 11:56:35 +0300

    fetched single property data on page load

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+"use client";
+import { fetchProperty } from "@/utils/requests";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const PropertyPage = () => {
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const FetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error("Error fetching property: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (property === null) {
+      FetchPropertyData();
+    }
+  }, [id, property]);
+
   return <div>PropertyPage</div>;
 };
 
