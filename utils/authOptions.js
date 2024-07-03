044ca268facb9870dc74675e403701906bf5911d
Abbas <abbas_kanj@hotmail.com>
2024-07-03 13:18:28 +0300

    added the sigin option for user to signin by google and creating new user if not exists/ created a session for each user signin

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   providers: [
@@ -16,7 +18,32 @@ export const authOptions = {
   ],
   callbacks: {
     //Invoked on sucessful sigin
-    async signIn({ profile }) {},
-    async session({ session }) {},
+    async signIn({ profile }) {
+      // 1. Connect to database
+      await connectDB();
+      // 2. Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
+      // 3. If not, then add user to database
+      if (!userExists) {
+        // Truncate username if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.image,
+        });
+      }
+      // 4. Return true to allow sigin
+      return true;
+    },
+    async session({ session }) {
+      // 1. Get user from database
+      const user = await User.findOne({ email: session.user.email });
+      // 2. Assign the user id to the session
+      session.user.id = user._id.toString();
+      // 3. return session
+      return session;
+    },
   },
 };
