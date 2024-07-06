dfec7d991f6efc21c513deac958325ef4d974a28
Abbas <abbas_kanj@hotmail.com>
2024-07-06 15:08:26 +0300

    modified the fetch all properties function to not store cache on browser to update new data fetched

--- utils/requests.js
+++ utils/requests.js
@@ -3,10 +3,11 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 // Fetch all properties
 async function fetchProperties() {
   try {
+    // Handle the case where the domain is no available yet
     if (!apiDomain) {
       return [];
     }
-    const res = await fetch(`${apiDomain}/properties`);
+    const res = await fetch(`${apiDomain}/properties`, { cache: "no-store" });
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
