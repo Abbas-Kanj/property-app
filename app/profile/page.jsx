a8232dc6a0a6de5a516911dd0900c2988dda8b1c
Abbas <abbas_kanj@hotmail.com>
2024-07-06 23:08:18 +0300

    added a section to map over the listings of the user

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import profileDefault from "@/assets/images/profile.png";
+import Spinner from "@/components/Spinner";
 
 const ProfilePage = () => {
   return (
@@ -27,6 +29,55 @@ const ProfilePage = () => {
               <h2 className="text-2xl">
                 <span className="font-bold block">Email: </span> {profileEmail}
               </h2>
+            </div>
+
+            <div className="md:w-3/4 md:pl-4">
+              <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
+              {!loading && properties.length === 0 && (
+                <p>You have no property listings</p>
+              )}{" "}
+              {loading ? (
+                <Spinner loading={loading} />
+              ) : (
+                properties.map((property) => (
+                  <div key={property._id} className="mb-10">
+                    <Link href={`/properties/${property._id}`}>
+                      <Image
+                        className="h-32 w-full rounded-md object-cover"
+                        src={property.images[0]}
+                        width={500}
+                        height={100}
+                        priority={true}
+                        alt="Property Image"
+                      />
+                    </Link>
+                    <div className="mt-2">
+                      <p className="text-lg font-semibold">{property.name}</p>
+                      <p className="text-gray-600">
+                        Address: {property.location.street}
+                        {property.location.city} {property.location.state}
+                      </p>
+                    </div>
+                    <div className="mt-2">
+                      <Link
+                        href={`/properties/${property._id}/edit`}
+                        className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+                        type="button"
+                        onClick={() => {
+                          handleDeleteProperty(property._id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
