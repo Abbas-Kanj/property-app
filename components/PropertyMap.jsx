5a20251e91d2d02c393aca630a76236ec5d52e9c
Abbas <abbas_kanj@hotmail.com>
2024-07-09 06:46:57 +0300

    created an propertyMap component

---
+++ components/PropertyMap.jsx
@@ -0,0 +1,32 @@
+"use client";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { Marker } from "react-map-gl";
+import { setDefaults, fromAddress } from "react-geocode";
+import pin from "@/assets/images/pin.svg";
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
+import Image from "next/image";
+
+const PropertyMap = ({ property }) => {
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import("mapbox-gl")}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: "100%", height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
+};
+
+export default PropertyMap;
