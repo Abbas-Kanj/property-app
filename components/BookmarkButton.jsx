4e112312c0b029d62072e6283569c15dce4b74f1
Abbas <abbas_kanj@hotmail.com>
2024-07-09 13:15:12 +0300

    added a turnery to check the state if it is bookmarked and renendering accordingly

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -37,7 +37,14 @@ const BookmarkButton = ({ property }) => {
       toast.error("Something went wrong");
     }
   };
-  return (
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Remove Property
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
