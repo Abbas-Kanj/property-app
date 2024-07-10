c9c905a5f1676ace043d567c883b4e338a1f5d4e
Abbas <abbas_kanj@hotmail.com>
2024-07-10 11:46:32 +0300

    created a search results page and fetched properties successfully

---
+++ app/properties/search-results/page.jsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+const SearchResultsPage = () => {
+  const searchParams = useSearchParams();
+
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const location = searchParams.get("location");
+  const propertyType = searchParams.get("propertyType");
+
+  useEffect(() => {
+    const setPropertiesResult = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data);
+        } else {
+          setProperties([]);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    setPropertiesResult();
+  }, [location, propertyType]);
+
+  console.log(properties);
+
+  return <div>SearchResultsPage</div>;
+};
+
+export default SearchResultsPage;
